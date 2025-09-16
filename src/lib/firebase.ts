75ac445ac8d0118a23f2c3aeccee91b1d306d022
foodietrusts-cmd <foodietrusts@gmail.com>
2025-09-15 20:45:05 -0500

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,23 +1,21 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+// src/lib/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBjeFGDRdOtINeLTdN2kwCIZmo8P7ludO0",
-  authDomain: "foodietrust-staging.firebaseapp.com",
-  projectId: "foodietrust-staging",
-  storageBucket: "foodietrust-staging.firebasestorage.app",
-  messagingSenderId: "317774032756",
-  appId: "1:317774032756:web:ceadf3bf77180a1adb4621",
-  measurementId: "G-T1XZWTB7GS"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
 
-export { auth, db, storage };
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
